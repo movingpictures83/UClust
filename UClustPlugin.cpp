08c6e5488cc2a0b21dfeb56aebcf0f5d157b9952
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-07-27 17:51:40 -0400

    Removed Python2 paths

--- UClustPlugin.cpp
+++ UClustPlugin.cpp
@@ -13,9 +13,11 @@ void UClustPlugin::run() {
 
 void UClustPlugin::output(std::string file) {
 //beta_diversity.py -i filtered_otu_table.biom -m euclidean,weighted_unifrac,unweighted_unifrac -t rep_set.tre -o beta_div
-   std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; pick_otus.py ";
+   //std::string command = "export OLDPATH=${PYTHONPATH}; export PYTHONPATH=${PYTHON2_DIST_PACKAGES}:${PYTHON2_SITE_PACKAGES}:${PYTHONPATH}; pick_otus.py ";
+   std::string command = "pick_otus.py ";
  command += "-i "+inputfile+" ";
- command += "-o "+file+"; cp "+file+"/*.uc "+file+"/..; cp "+file+"/*.txt "+file+"/..; export PYTHONPATH=OLDPATH";
+ //command += "-o "+file+"; cp "+file+"/*.uc "+file+"/..; cp "+file+"/*.txt "+file+"/..; export PYTHONPATH=OLDPATH";
+ command += "-o "+file+"; cp "+file+"/*.uc "+file+"/..; cp "+file+"/*.txt "+file+"/..";
  std::cout << command << std::endl;
 
  system(command.c_str());
